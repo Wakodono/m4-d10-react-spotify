ed09e73cd24d4926f4f9c3984c36e816e0d823ea
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-09-03 12:22:24 +0100

    Starting point

---
+++ react-app/src/Components/HomepageContent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HomepageContent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
