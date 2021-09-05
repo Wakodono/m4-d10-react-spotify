e43bedb4e19e0e7b040855f0c6593501e7ea46f5
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-09-05 16:15:43 +0100

    Header component

---
+++ react-app/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className="header__wrapper">
+            <span>Trending</span>
+            <span>Podcast</span>
+            <span>Mood and genres</span>
+            <span>New releases</span>
+            <span>Discover</span>
+        </div>
+    )
+}
--- react-app/src/views/HomePage/HomePage.jsx
+++ react-app/src/views/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import SingleAlbum from '../../components/SingleAlbum'
 import './HomePage.css'
 
 import { fetchAlbum } from '../../apicalls'
+import Header from '../../components/header/Header'
 
 const HomePage = () => {
     const [skepta, setskepta] = useState([])
@@ -13,12 +14,17 @@ const HomePage = () => {
         fetchAlbum("fireboy dml").then((res) => setfireboy(res) )
     }, [])
     return <div className="home__wrapper">
+        <Header />
         {/* {section 1} */}
-        <h2>Time machine</h2>
-        {skepta && skepta.slice(0,6).map((song) => <SingleAlbum src={song.album.cover_medium} key={song.id} />)}
+            <h2>Time machine</h2>
+        <div className="home__line">
+            {skepta && skepta.slice(0,6).map((song) => <SingleAlbum src={song.album.cover_medium} key={song.id} />)}
+        </div>
         {/* {section 2} */}
-        <h2>New releases</h2>
-        {fireboy && fireboy.slice(0,6).map((song) => <SingleAlbum src={song.album.cover_medium} key={song.id} />)}
+            <h2>New releases</h2>
+        <div className="home__line">
+            {fireboy && fireboy.slice(0,6).map((song) => <SingleAlbum src={song.album.cover_medium} key={song.id} />)}
+        </div>
     </div>
 }
 
