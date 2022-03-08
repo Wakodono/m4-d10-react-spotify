6ad7817b001bc101c407dee9a7ff31aadded7efe
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-03-08 17:49:10 +0000

    Updates SingleSong

--- react-app/src/components/SingleSong/SingleSong.jsx
+++ react-app/src/components/SingleSong/SingleSong.jsx
@@ -5,7 +5,7 @@ import {CgMusicNote} from "react-icons/cg"
 export default function SingleSong({title, artist, duration}) {
     return (
         <div className="single__wrapper">
-           <div className="note-icon">CgMusicNote</div> 
+           <div className="note-icon">{CgMusicNote}</div> 
            <div className="title-and-artist">
                 <span className="title">{title}</span>   
                 <span className="artist">{artist}</span>   
