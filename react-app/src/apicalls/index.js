eeef22ecc45148a72edf58866e9e5c51b7b5ecca
Wakodono <53127265+Wakodono@users.noreply.github.com>
2022-03-11 16:42:18 +0000

    Adds ArtistPage and fetchArtist apicall

--- react-app/src/apicalls/index.js
+++ react-app/src/apicalls/index.js
@@ -9,3 +9,18 @@ export const fetchAlbum = async(id) => {
     let album = await response.json()
     return album
 }
+
+export const fetchArtist = async(id) => {
+    let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${id}`)
+    let artist = await response.json()
+    let url = artist.tracklist
+    let track_res = await fetch(url)
+    let tracks = await track_res.json()
+    let albums = tracks.data.map((song)=> song.album )
+    artist = {
+        ...artist,
+        tracklist: tracks.data,
+        albums: albums
+    }
+    return artist
+}
--- react-app/src/views/ArtistPage/ArtistPage.jsx
+++ react-app/src/views/ArtistPage/ArtistPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-export default function ArtistPage() {
+import { fetchArtist } from "../../apicalls";
+import SingleAlbum from "../../components/SingleAlbum/SingleAlbum";
+import Button from "../../components/Button/Button"
+import SingleSong from "../../components/SingleSong/SingleSong"
+import "./ArtistPage.css";
+
+const ArtistPage = (props) => {
+    const [artist, setArtist] = useState({})
+
+    useEffect(() => {
+      fetchArtist(props.match.params.id).then((res) => setArtist(res))
+    
+    }, [])
+    
+
     return (
-        <div>
-            
+        <div className="artist-page__wrap">
+            <img src={artist.picture_xl} alt={artist.name} className="artist__header" />
+            <div className="header__wrap--art">
+                <div className="listeners">{artist?.nb_fan} MONTHLY LISTENERS</div>
+                <h1>{artist?.name}</h1>
+                <div>
+                    <Button text="Play" />
+                    <button className="follow__btn">Sign up</button>
+                </div>
+            </div>
+            <h2>Albums</h2>
         </div>
     )
 }
+
+export default ArtistPage
+
